bc343a17cf9f62b58236837f0cba1b065f39a1fe
Anastasia Korolkova <kkkkkkkkk72@yandex.ru>
2022-12-02 20:46:34 +0300

    task1.3commit

--- task_1/solution.js
+++ task_1/solution.js
@@ -26,7 +26,7 @@ function addInCartCountChange(value) {
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
     let newValue = oldValue + 1;
-    
+
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -42,6 +42,12 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
+    let newSum = oldSum + difference;
+    let newSumText = `${newSum} P`;
+    
+    
+
+
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
