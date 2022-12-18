cd1d149a22b7425a4ff5aba6cab7c515dbde4801
Anastasia Korolkova <kkkkkkkkk72@yandex.ru>
2022-12-18 17:07:34 +0300

    finaltaskfinalcommit

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,24 +33,20 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: `${name} ${phone}`, order: {address, sum}, goods: []};
-
-    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;    
-    data.order.sum = sum;
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i < countOfGoods; i++) {                
-        let goodsItem = {title: goods[i].title, count: goods[i].count};        
-        data.goods.push(goodsItem); 
+function sendRequest(name, phone, {street, house, entrance, floor, flat}, goods, sum) {
+    let data = {
+        client: name + " " + phone,
+        order: {
+            address: "ул. " + street + ", дом " + house + ", " + entrance + " подъезд, " + floor + " этаж," + " кв. " + flat,
+            sum: sum
+        },
+        goods: []
     }
 
-    dataObject = {data};
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(dataObject); 
-
-    return jsonData;
+    let countOfGoods = goods.length;
+    for (let i = 0; i < countOfGoods; i++) {
+    data.goods.push({title:goods[i].title, count: goods[i].count});
+}
+let jsonData = JSON.stringify({data});
+return jsonData
 }
